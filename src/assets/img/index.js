d9fe188e856b4451501c8137a4d3734bd9e4433d
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-03 15:47:46 +0900

    [Page] /home (/main) add Box 📲

---
+++ src/assets/img/index.js
@@ -0,0 +1,57 @@
+import Email from './components/email.svg';
+import Git from './components/git.svg';
+
+import HomeCarouselOsaka from './pages/home/carousel-osaka.jpg';
+import HomeCarouselBangkok from './pages/home/carousel-bangkok.jpg';
+import HomeCarouselDanang from './pages/home/carousel-danang.jpg';
+import HomeCarouselFlorence from './pages/home/carousel-florence.jpg';
+import HomeHelp from './pages/home/help.svg';
+import HomeCamera from './pages/home/camera.svg';
+import HomeHanger from './pages/home/hanger.svg';
+import HomeFood from './pages/home/food.svg';
+
+const Images = {
+    Email: {
+        name: 'email',
+        path: Email,
+    },
+    Git: {
+        name: 'git',
+        path: Git
+    },
+
+    HomeCarouselOsaka: {
+        name: 'osaka',
+        path: HomeCarouselOsaka
+    },
+    HomeCarouselBangkok: {
+        name: 'bangkok',
+        path: HomeCarouselBangkok
+    },
+    HomeCarouselDanang: {
+        name: 'danang',
+        path: HomeCarouselDanang
+    },
+    HomeCarouselFlorence: {
+        name: 'florence',
+        path: HomeCarouselFlorence
+    },
+    HomeHelp: {
+        name: 'help',
+        path: HomeHelp
+    },
+    HomeCamera: {
+        name: 'skyblue',
+        path: HomeCamera
+    },
+    HomeHanger: {
+        name: 'hanger',
+        path: HomeHanger
+    },
+    HomeFood: {
+        name: 'food',
+        path: HomeFood
+    },
+}
+
+export default Images;
