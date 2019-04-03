b301d5b5ab7038807391a1560217dcce8c31d238
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-03 15:28:18 +0900

    [INITIAL] App.js (Header, Pages, Footer) Imported 🔨

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import {
-  Home,
-
-  Cities,
-  City,
-
-  Countries,
-  Country,
-
-  Analysis,
-
-  Error
-} from './pages';
+import { Header, Footer } from './components';
+import Pages from './pages';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/cities' component={Cities} />
-          <Route exact path='/cities/:index' component={City} />
-          <Route exact path='/countries' component={Countries} />
-          <Route exact path='/countries/:index' component={Country} />
-          <Route exact path='/analysis' component={Analysis} />
-          <Route path='/' component={Error} />
-        </Switch>
-      </Router>
+      <>
+        <Header />
+        <Pages />
+        <Footer />
+      </>
     );
   }
 }
