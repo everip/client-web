9897ab94bb1cb9336eeea96bdfa29fc80d0f9b9f
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-03 15:42:32 +0900

    [Component] Footer add 📲

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import '../assets/css/components/footer.css';
+
+export default class Footer extends Component {
+    render = () => {
+        return (
+            <footer className='footer'>
+                Footer
+            </footer>
+        );
+    }
+}
