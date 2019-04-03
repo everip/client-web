af0b89658e0c8963c16e170d9687d0b3b5d76b5a
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-03 15:47:14 +0900

    [Page] /home (/main) add Carousel 📲

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,78 @@
 import React, { Component } from 'react';
 
+import { Carousel } from '../../components';
+
+import Images from '../../assets/img';
+
+import Box from './Box';
+
+import '../../assets/css/pages/home/home.css';
+
 export default class Home extends Component {
     render = () => {
         return (
-            <div>
-                Home
+            <div className="home">
+                <div className='main-carousel'>
+                    <Carousel
+                        data={[
+                            {
+                                item: Images.HomeCarouselOsaka,
+                                content:
+                                    <>
+                                        <div className='head'>PLANNING A TRIP<br />TO OSAKA?</div>
+                                        <div className='body'>The fool wanders, a wise man travels.<br />- Thomas Fuller -</div>
+                                    </>,
+                            },
+                            {
+                                item: Images.HomeCarouselBangkok,
+                                content:
+                                    <>
+                                        <div className='head'>PLANNING A TRIP<br />TO BANGKOK?</div>
+                                        <div className='body'>The fool wanders, a wise man travels.<br />- Thomas Fuller -</div>
+                                    </>,
+                            },
+                            {
+                                item: Images.HomeCarouselDanang,
+                                content:
+                                    <>
+                                        <div className='head'>PLANNING A TRIP<br />TO DANANG?</div>
+                                        <div className='body'>The fool wanders, a wise man travels.<br />- Thomas Fuller -</div>
+                                    </>,
+                            },
+                            {
+                                item: Images.HomeCarouselFlorence,
+                                content:
+                                    <>
+                                        <div className='head'>PLANNING A TRIP<br />TO FLORENCE?</div>
+                                        <div className='body'>The fool wanders, a wise man travels.<br />- Thomas Fuller -</div>
+                                    </>,
+                            },
+                        ]}
+                    />
+                </div>
+
+                <div className='wrapper'
+                    style={{
+                        width: '100%',
+                        height: '25rem'
+                    }}
+                >
+                    <Box
+                        icon={Images.HomeHelp}
+                        content={{
+                            head: <p>We make you happy</p>,
+                            body: <p>It provides customized travel information for you <br /> and is available free of charge.</p>,
+                        }}
+                    />
+                </div>
+
+
+                {/* <div className=''>
+                    <img src={Images.HomeHelp.path} alt={Images.HomeHelp.name} />
+                    <img src={Images.HomeCamera.path} alt={Images.HomeCamera.name} />
+                    <img src={Images.HomeHanger.path} alt={Images.HomeHanger.name} />
+                    <img src={Images.HomeFood.path} alt={Images.HomeFood.name} />
+                </div> */}
             </div>
         );
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { Home } from './home';
 import { Cities, City, Countries, Country } from './recommend';
 import { Analysis } from './analysis';
 import { Error } from './error';
 
-export {
-    Home,
+import '../assets/css/index.css';
+import '../assets/css/reset.css';
 
-    Cities,
-    City,
-    Countries,
-    Country,
-
-    Analysis,
-
-    Error
+export default class Pages extends Component {
+    render() {
+        return (
+            <article>
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Home} />
+                        <Route exact path='/cities' component={Cities} />
+                        <Route exact path='/cities/:index' component={City} />
+                        <Route exact path='/countries' component={Countries} />
+                        <Route exact path='/countries/:index' component={Country} />
+                        <Route exact path='/analysis' component={Analysis} />
+                        <Route path='/' component={Error} />
+                    </Switch>
+                </Router>
+            </article>
+        );
+    }
 }
