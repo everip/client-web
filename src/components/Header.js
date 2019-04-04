859f38a21df2f9800eee97469e895a50e11940d2
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-04 12:52:22 +0900

    [Component] Header 수정 🔨

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ import '../assets/css/components/header.css';
 export default class Header extends Component {
     render = () => {
         return (
-            <header className='home-header'>
+            <header className='header'>
                 <div className='header-logo'>
-                    LOGO
+                    EVERIP
                 </div>
                 <div className='header-links'>
                     <img src={Images.Email.path} alt={Images.Email.name} />
