0f6dc0fa42f40491a1c517e669cc537beb3522ed
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-05 18:38:47 +0900

    [Component] Footer edit 🔨

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,17 +7,17 @@ export default class Footer extends Component {
     render = () => {
         return (
             <footer
-                className='footer'
+                className={`footer`}
                 style={{
                     backgroundImage:
                         `linear-gradient(rgba(0, 0, 0, 1),  rgba(0, 0, 0, 1)), url(${Images.Footer.path})`
                 }}
             >
-                <div className='footer-back'></div>
-                <div className='footer-overlay'>
-                    <div className='footer-content'>
-                        <div className='copyright'>©EVERIP 2019. All Right Reserved</div>
-                        <div className='links'>
+                <div className={`footer-back`}></div>
+                <div className={`footer-overlay`}>
+                    <div className={`footer-content`}>
+                        <div className={`copyright`}>©EVERIP 2019. All Right Reserved</div>
+                        <div className={`links`}>
                             <span onClick={() => { window.open('https://www.facebook.com/profile.php?id=100008311933745') }}>Facebook</span>
                             <span onClick={() => { window.open('https://github.com/trip-wherever-you-want-go') }}>GitHub</span>
                         </div>
