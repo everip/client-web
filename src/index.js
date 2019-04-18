d1a063786b428742c6523d1f29121004abbd570c
Yonghyun Kim <sociablesasha@gmail.com>
2019-04-18 18:07:44 +0900

    FontAwesome Icon 추가 🖼

--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+    faChevronLeft,
+    faChevronRight,
+    faHeart,
+    faInfo,
+    faMapMarked,
+    faUtensils,
+    faCarSide,
+    faHashtag,
+    faHandPointRight,
+    faCloudSunRain,
+    faEquals,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+
+} from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+    faChevronLeft,
+    faChevronRight,
+    faHeart,
+    faInfo,
+    faMapMarked,
+    faUtensils,
+    faCarSide,
+    faHashtag,
+    faHandPointRight,
+    faCloudSunRain,
+    faEquals,
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
